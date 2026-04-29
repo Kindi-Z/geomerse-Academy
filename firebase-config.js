640a0ed574cd504fc11e6b4399c00b0ec354abf7
upzaxe <iamkhaliv@gmail.com>
2026-04-29 08:00:12 +0700

    v

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,27 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxwyTe5FylqJZcPO8TA7Va_xCRbMzJyWk",  
-  authDomain: "geomers-academy-5b9e5.firebaseapp.com", 
-  projectId: "geomers-academy-5b9e5", 
-  storageBucket: "geomers-academy-5b9e5.firebasestorage.app", 
-  messagingSenderId: "598328922866", 
-  appId: "1:598328922866:web:d2f38f330c53e9d227766b" 
+  apiKey: "AIzaSyCxwyTe5FylqJZcPO8TA7Va_xCRbMzJyWk",
+  authDomain: "geomers-academy-5b9e5.firebaseapp.com",
+  projectId: "geomers-academy-5b9e5",
+  storageBucket: "geomers-academy-5b9e5.firebasestorage.app",
+  messagingSenderId: "598328922866",
+  appId: "1:598328922866:web:d2f38f330c53e9d227766b"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export {
+  app,
+  auth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+};
